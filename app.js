956fef2b0a971e6be8f1bf52f7d2e192cffe5643
Edécio Fernando Iepsen <edeciofernando@gmail.com>
2022-05-10 20:09:45 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const express = require('express');
 const app = express();
 
@@ -16,4 +17,24 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
     console.log(`Servidor em execução na porta: ${port}`);
+=======
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+const routes = require("./routes");
+
+// middleware para aceitar dados no formato JSON 
+app.use(express.json());
+
+app.use(routes);
+
+app.get('/', (req, res) => {
+    res.send('Programação Web. Revenda de Veículos');
+})
+
+app.listen(port, () => {
+    console.log(`Servidor em execução na porta: ${port}`);
+>>>>>>> d5065469abd1908003c02358998abc0d8343b99f
 })
