d5065469abd1908003c02358998abc0d8343b99f
Edécio Fernando Iepsen <edeciofernando@gmail.com>
2022-05-10 14:28:54 -0300

    Heroku v.1

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+
+const port = process.env.PORT || 3000;
 
 const routes = require("./routes");
 
@@ -16,4 +17,3 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Servidor em execução na porta: ${port}`);
 })
-
